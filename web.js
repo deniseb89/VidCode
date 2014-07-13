3bd59ad2a14234ff65904c46d7dd3da21d84ddeb
Leandra Tejedor <lmtejedor@gmail.com>
2014-07-12 20:36:14 -0400

    Add a template for lessons

--- web.js
+++ web.js
@@ -64,6 +64,13 @@ app.get('/demo/:token?', routes.demo(db));
 app.get('/demo2', routes.demo2(db));
 app.get('/gallery', routes.gallery);
 app.get('/galleryshow', routes.galleryshow);
+
+//------lesson template ----------//
+app.get('/lesson/1', routes.partone);
+app.get('/lesson/2', routes.parttwo);
+app.get('/lesson/3', routes.partthree);
+app.get('/lesson/4', routes.partfour);
+
 app.get('/codeAlone', routes.codeAlone);
 app.post('/upload', routes.upload);
 app.post('/save', routes.save(db, crypto));
