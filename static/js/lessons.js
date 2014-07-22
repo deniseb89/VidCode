b60d8eebf152ad783cf3ab6ea53a7822bb029d99
Leandra Tejedor <lmtejedor@gmail.com>
2014-07-21 22:20:05 -0400

    one page s-s-s-slide

--- static/js/lessons.js
+++ static/js/lessons.js
@@ -7,4 +7,34 @@ $( document ).ready(function() {
 		$('.js-current-vid').attr('src', thisSrc);
 	});
 
+  $('.js-slide-right-final').click(function(){
+  	slideRight('.js-slide-1', '.js-slide-final');
+  });
+
+  $('.js-slide-left-first').click(function(){
+  	slideLeft('.js-slide-1', '.js-slide-final');
+  });
+
 });
+
+
+var slideLeft = function(oldSlide, newSlide){
+	$(newSlide).addClass('is-hidden');
+	$(oldSlide).removeClass('is-hidden');
+	$(oldSlide).animate({
+    opacity: 1,
+    'margin-left': "0px"
+    });
+}
+
+var slideRight = function(oldSlide, newSlide){
+	$(oldSlide).animate({
+    'margin-left': "-200px",
+    opacity: 0
+    }, function() {
+      $(oldSlide).addClass('is-hidden');
+    });
+  setTimeout(function(){
+  	$(newSlide).removeClass('is-hidden');
+  }, 500);
+}
