a100066db47ab0ad6f88851e9efb1b79285d3ba2
msmahliss <msmahliss@gmail.com>
2014-07-29 11:31:45 -0500

    local vars for staging env

--- web.js
+++ web.js
@@ -48,9 +48,6 @@ passport.use(new InstagramStrategy({
     clientID: config.INSTAGRAM_CLIENT_ID_LOCAL,
     clientSecret: config.INSTAGRAM_CLIENT_SECRET_LOCAL,
     callbackURL: config.INSTAGRAM_CB_LOCAL
-    // clientID: config.INSTAGRAM_CLIENT_ID,
-    // clientSecret: config.INSTAGRAM_CLIENT_SECRET,    
-    // callbackURL: "config.INSTAGRAM_CB
   },
   function(accessToken, refreshToken, profile, done) {
     // asynchronous verification, for effect...
