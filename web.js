97320430ad930a9fead36f3f9c5fb6e65d1c4a12
Adam J. Mendoza <adammendoza@hotmail.com>
2015-01-05 08:46:36 -0800

    Update express-handlebars

--- web.js
+++ web.js
@@ -1,6 +1,6 @@
 // required packages  ==========================================================
 var express = require('express');
-var exphbs = require('express3-handlebars');
+var exphbs = require('express-handlebars');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
